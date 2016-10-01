124cc34116e3eab605ece8a2c5b4721b4f5e8a40
inawayyoullneverknow <789dash@gmail.com>
2016-10-01 19:30:44 +0500

    Update demo
    Downloading avatars was buggy (not plugin's fault) so I removed it until
    I find a way to fix it.

--- GJAPI Demo/main.gd
+++ GJAPI Demo/main.gd
@@ -2,13 +2,14 @@ extends Node
 
 var trophies_list
 var trophy_no = 0
+var gj
 
 func _ready():
-	get_node("gjapi").connect("api_authenticated", self, "on_gj_auth")
-	get_node("gjapi").connect("api_user_fetched", self, "on_gj_fetch_user")
-	get_node("avatar").connect("request_completed", self, "avatar_done")
-	get_node("gjapi").connect("api_session_opened", self, "on_session_opened")
-	get_node("gjapi").connect("api_trophy_fetched", self, "trophies_fetched")
+	gj = get_node("gjapi")
+	gj.connect("api_authenticated", self, "on_gj_auth")
+	gj.connect("api_user_fetched", self, "on_gj_fetch_user")
+	gj.connect("api_session_opened", self, "on_session_opened")
+	gj.connect("api_trophy_fetched", self, "trophies_fetched")
 	pass
 	
 func trophies_fetched(data):
@@ -27,7 +28,7 @@ func on_session_opened(data):
 	pass
 
 func _on_auth_pressed():
-	get_node("gjapi").auth_user(get_node("ui/token").get_text(), get_node("ui/username").get_text())
+	gj.auth_user(get_node("ui/token").get_text(), get_node("ui/username").get_text())
 	pass # replace with function body
 
 func on_gj_auth(data):
@@ -35,13 +36,7 @@ func on_gj_auth(data):
 	get_node("ui/close_s").set_disabled(false)
 	get_node("ui/list_trophies").set_disabled(false)
 	print(data)
-	get_node("gjapi").fetch_user_by_name("Rainforest")
-	pass
-	
-func avatar_done(result, response_code, headers, body):
-	var a = load("res://avatar.jpg")
-	get_node("ui/container/avatar").set_texture(a)
-	get_node("ui/container/my_avatar").show()
+	gj.fetch_user_by_name(gj.get_username())
 	pass
 	
 func on_gj_fetch_user(data):
@@ -51,23 +46,20 @@ func on_gj_fetch_user(data):
 	d = d["response"]["users"][0] # the api outputs everything in one dictionary so fetch it
 	for i in d.keys(): # get all information
 		res.set_text(res.get_text() + "\n" + i + ": " + d[i])
-	get_node("avatar").set_download_file("res://avatar.jpg")
-	get_node("avatar").request(d["avatar_url"].replace("https", "http"))
-	get_node("gjapi").add_score_for_guest("Whoa", 50, "TestGuest1234")
 	pass
 
 func _on_open_s_pressed():
-	get_node("gjapi").open_session()
+	gj.open_session()
 	pass # replace with function body
 
 
 func _on_close_s_pressed():
-	get_node("gjapi").close_session()
+	gj.close_session()
 	pass # replace with function body
 
 
 func _on_list_trophies_pressed():
-	get_node("gjapi").fetch_trophies()
+	gj.fetch_trophies()
 	pass # replace with function body
 
 
